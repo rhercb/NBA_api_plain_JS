83ff0147356354ad0da578d52e17bcabce189dff
rhercb <rhercb@gmail.com>
2020-12-31 02:08:32 +0200

    Get base workspace to run on npm start

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
@@ -6,5 +7,19 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist/js"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
   mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
