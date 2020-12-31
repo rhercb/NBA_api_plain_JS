33ce6dc7f0ce00726db58268698689afbe68cf0f
rhercb <rhercb@gmail.com>
2020-12-31 13:44:30 +0200

    Add timeout and AJAX function for API calls to helper.js

---
+++ src/js/helper.js
@@ -0,0 +1,35 @@
+/**
+ * Helper consists of multiple helper functions that are used across whole APP
+ */
+
+/**
+ * Creates a timouet after x sec have gone by
+ * @param {number} s max amout of secounds for request to process
+ */
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+/**
+ * AJAX function creates a async function that requests data from API URL
+ * @param {string} url Url of API cann
+ */
+export const AJAX = async function (url) {
+  try {
+    const fetchPro = fetch(url);
+
+    // Promise.race provides a race between API reuest, and timout request.
+    // If API request doesnt end after timeout(x) secounds, the request will end and show error
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  } catch (err) {
+    throw err;
+  }
+};
