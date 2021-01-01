2d65932964a453f7014fafb5a7d2613433a8d14e
rhercb <rhercb@gmail.com>
2021-01-01 19:57:24 +0200

    Style teams view

--- src/js/views/teamsView.js
+++ src/js/views/teamsView.js
@@ -1,7 +1,7 @@
 import View from "./View";
 
 class TeamsView extends View {
-  _parentElement = document.querySelector(".body");
+  _parentElement = document.querySelector(".body__teams");
 
   _generateMarkup() {
     return this._data
@@ -10,7 +10,17 @@ class TeamsView extends View {
   }
 
   _generateSingleMarkup(data) {
-    return `<div>${data.city}</div>`;
+    return `<div data-team-key="${data.key}" class="team__wrapper col-12" style="background-color: #${data.primaryColor}70">
+                <div class="team__image">
+                    <img src="${data.logo}"/>
+                </div>
+                <div class="team__info">
+                    <div class="team__name">
+                        <p class="team__name--city">${data.city}</p>
+                        <p class="team__name--name">${data.name}</p>
+                    </div>
+                </div>
+            </div>`;
   }
 }
 
